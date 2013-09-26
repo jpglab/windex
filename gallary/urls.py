8199e615c24095dc41241f2c295715d242e3067c
John Rork <johnrork@sd54.org>
2013-09-26 17:02:31 -0500

    Sigh

--- gallary/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from gallary import views
-
-urlpatterns = patterns('',
-    # domain.com/gallary
-    url(r'^$', views.gallary, name='gallary'),
-)
--- gallary/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def gallary(request):
-  return HttpResponse('Welcome to the gallary app!')
