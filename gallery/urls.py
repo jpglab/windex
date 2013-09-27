513824b1b9ca410b5405aca51d209019b9a2af91
John Paul Glab <johnpaulglab@gmail.com>
2013-09-26 17:12:36 -0700

    Changed more gallary --> gallery in import statements and urls.py. Added admin.py for the gallery app.

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
-from gallary import views
+from gallery import views
 
 urlpatterns = patterns('',
-    # domain.com/gallary
-    url(r'^$', views.gallary, name='gallary'),
+    # domain.com/gallery
+    url(r'^$', views.gallary, name='gallery'),
 )
