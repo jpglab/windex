9c9eaeb4a0a9fc353f5725e678801e8264a1e997
John Paul Glab <johnpaulglab@gmail.com>
2013-09-27 08:40:01 -0700

    Everything working as it should.

--- index/views.py
+++ index/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse('Welcome to Windex!')
+  return HttpResponse('<h2>Welcome to Windex!</h2><br/>This is the index app\'s root url.')
