3c40c0e6fb211cec05a68f7a544b89ccb7cb6d6b
John Paul Glab <johnpaulglab@gmail.com>
2013-09-26 17:22:31 -0700

    IDKWTFBBQ

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,9 +3,5 @@ from gallery import views
 
 urlpatterns = patterns('',
     # domain.com/gallery
-<<<<<<< HEAD
     url(r'^$', views.gallery, name='gallery'),
-=======
-    url(r'^$', views.gallary, name='gallery'),
->>>>>>> 513824b1b9ca410b5405aca51d209019b9a2af91
 )
