f989c0a6632feb8a656d94e5ed37215a81fbffc5
John Paul Glab <johnpaulglab@gmail.com>
2013-09-30 17:31:07 -0700

    Added media url.

--- windex/urls.py
+++ windex/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
+
+#Development only
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = patterns('',
     url(r'^$', include('index.urls')),
     url(r'^events/', include('index.urls')), 
     url(r'^gallery/', include('gallery.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
