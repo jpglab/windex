bac68f8930a46a115f10288c85c4abd12f728606
John Paul Glab <johnpaulglab@gmail.com>
2013-10-18 10:13:50 -0700

    Added the Album model. Every image will have a fk album.

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+class Album(models.Model):
+  name = models.CharField(max_length=100, blank=False)
+  description = models.TextField()
+
+  def __unicode__(self):
+    return self.name
+
+
 class Image(models.Model):
+  album = models.ForeignKey(Album)
   path = models.ImageField(upload_to='images/', name='Image', null=False)
   caption = models.CharField(max_length=32, blank=False)
   date = models.DateTimeField(auto_now_add=True)
