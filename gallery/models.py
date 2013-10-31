56684ee00a14d582c337c8eecea7cf1233f1a791
John Paul Glab <johnpaulglab@gmail.com>
2013-10-31 08:22:15 -0700

    Editied gallery model field names.

--- gallery/models.py
+++ gallery/models.py
@@ -4,17 +4,18 @@ from django.db import models
 from PIL import Image as PIL_Image
 
 def to_thumb(img):
+  "Creates a thumbnail of an uploaded image."
   size = 480, 480
 
   #Split the ImageField's name and insert .thumb into it
-  #Assign the filename to the thumb_path var to use when saving the final thumbnail image
+  #Assign the filename to thumb_path to use when saving the final thumbnail image
   filename, ext = os.path.splitext(img.name)
   thumb_path = filename + '.thumb.jpg'
 
   #Open image associated with the ImageField
   i = PIL_Image.open(windex.settings.MEDIA_ROOT + img.name)
 
-  #Create thumnail
+  #Create thumnail and safe it to the thumb_path
   i.thumbnail(size, PIL_Image.ANTIALIAS)
   i.save(windex.settings.MEDIA_ROOT + thumb_path, 'JPEG')
 
@@ -37,17 +38,17 @@ class Album(models.Model):
 
 class Image(models.Model):
   album = models.ForeignKey(Album)
-  file_path = models.ImageField(upload_to='images/image-images', name='Image', null=False)
-  thumb_path = models.ImageField(upload_to='images/image-images', blank=True, editable=False)
-  caption = models.CharField(max_length=32, blank=False)
   date = models.DateTimeField(auto_now_add=True)
+  caption = models.CharField(max_length=32, blank=False)
+  image_image = models.ImageField(upload_to='images/image-images', null=False)
+  image_thumb = models.ImageField(upload_to='images/image-images', blank=True, editable=False)
 
   def __unicode__(self):
     return self.caption
 
   def save(self):
-    f, e = os.path.splitext(self.album_image.name)
-    self.album_thumb = 'images/image-images' + f.replace(' ','_')  + '.thumb.jpg'
-    super(Album, self).save()
-    to_thumb(self.album_image)
+    f, e = os.path.splitext(self.image_image.name)
+    self.image_thumb = 'images/image-images/' + f.replace(' ','_')  + '.thumb.jpg'
+    super(Image, self).save()
+    to_thumb(self.image_image)
     
